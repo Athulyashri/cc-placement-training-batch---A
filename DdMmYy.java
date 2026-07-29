1bc9ea9a498e9bff50dfd16a8e7c287a648756d9
Athulyashri <athulyashri57@gmail.com>
2026-07-29 10:07:23 +0530

    Add main method to DdMmYy.java

---
+++ DdMmYy.java
@@ -0,0 +1,7 @@
+public static void main(String[] args) {
+int d,m,y,sum =0;
+Scanner sc=new Scanner(System.in);
+	  System.out.println("Enter DDMMYY : ");
+d=sc.nextInt();
+m=sc.nextInt();
+y=sc.nextInt();
