6bd7861ac88b49ea88f6f732e86b2c3028fad4f2
Athulyashri <athulyashri57@gmail.com>
2026-07-29 15:47:35 +0530

    Add Main class to calculate number of rats

---
+++ Poison And Rat.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int bottles = sc.nextInt();
+
+        int rats = 0;
+        int power = 1;
+
+        while (power < bottles) {
+            power *= 2;
+            rats++;
+        }
+
+        System.out.println(rats);
+
+        sc.close();
+    }
+}
